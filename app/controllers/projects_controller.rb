ff73c4aee5640f9c8dc75e50658a883f933d6f2b
Tim Fleming <tfleming26@hotmail.com>
2018-06-23 16:50:30 -0600

    Add create action to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,14 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.create(project_params)
+
+    if @project.save
+      flash[:notice] = "The project has been created."
+      redirect_to @project
+    else
+      # notta yet
+    end
 end
