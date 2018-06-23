6b2b560535cbf9c447e3862f6fd8609b9830ce92
Tim Fleming <tfleming26@hotmail.com>
2018-06-23 17:48:45 -0600

    Add title functionality for project show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*contents)
+    unless contents.empty?
+      content_for :title do
+        (contents << "Ticketify").join(" - ")
+      end
+    end
+  end
 end
