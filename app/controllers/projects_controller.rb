e162efb2599e08b38b0456526b9834953333440d
Tim Fleming <tfleming26@hotmail.com>
2018-06-23 18:26:59 -0600

    Add check for invalid attributes with flash alert

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,8 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      # notta yet
+      flash.now[:alert] = "Project has not been created."
+      render "new"
     end
   end
 
