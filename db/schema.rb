45a1ce323e2f0d1c8c79a8d35b48999d75463783
Tim Fleming <tfleming26@hotmail.com>
2018-07-02 02:12:36 -0600

    Create ticket model and add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_23_221724) do
+ActiveRecord::Schema.define(version: 2018_07_02_080824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 2018_06_23_221724) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tickets_on_project_id"
+  end
+
+  add_foreign_key "tickets", "projects"
 end
