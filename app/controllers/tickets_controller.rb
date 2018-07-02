dfe445dd6ecac551ddde6eeec5d3bd4298265988
Tim Fleming <tfleming26@hotmail.com>
2018-07-02 13:20:28 -0600

    Add set ticket method to tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
   before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def new
     @ticket = @project.tickets.build
@@ -17,6 +18,9 @@ class TicketsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
     def ticket_params
@@ -25,5 +29,9 @@ class TicketsController < ApplicationController
     
     def set_project
       @project = Project.find(params[:project_id])
+    end
+
+    def set_ticket
+      @ticket = @project.tickets.find(params[:id])
     end
 end
