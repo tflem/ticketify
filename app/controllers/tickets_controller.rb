6c8cf653a93c5f19ac320b7bf67774cb781b36a7
Tim Fleming <tfleming26@hotmail.com>
2018-07-02 02:11:22 -0600

    Add set project method

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,13 @@
 class TicketsController < ApplicationController
+  before_action :set_project
 
-    def new
-      @ticket = @project.tickets.build
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  private
+    
+    def set_project
+      @project = Project.find(params[:project_id])
     end
 end
