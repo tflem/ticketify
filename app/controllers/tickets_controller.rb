e11e2c693878c78eee5fa059390bd8befef93a83
Tim Fleming <tfleming26@hotmail.com>
2018-07-02 12:03:51 -0600

    Add create action to tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,7 +5,23 @@ class TicketsController < ApplicationController
     @ticket = @project.tickets.build
   end
 
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been created."
+      render "new"
+    end
+  end
+
   private
+
+    def ticket_params
+      params.require(:ticket).permit(:name, :description)
+    end
     
     def set_project
       @project = Project.find(params[:project_id])
