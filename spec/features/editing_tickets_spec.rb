565bbf845bf7a8f789feac6124478a1afce205c7
Tim Fleming <tfleming26@hotmail.com>
2018-07-04 11:44:40 -0600

    Add update ticket functionality

---
+++ spec/features/editing_tickets_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.feature "Users can edit existing tickets" do
+  let(:project) { FactoryBot.create(:project) }
+  let(:ticket)  { FactoryBot.create(:ticket, project: project) }
+
+  before do
+    visit project_ticket_path(project, ticket)
+    click_link "Edit Ticket"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Name",	with: "Make it respond faster!"
+    click_button "Update Ticket"
+    
+    expect(page).to have_content "Ticket has been updated."
+
+    within("#ticket h2") do
+      expect(page).to have_content "Make it respond faster!"
+      expect(page).not_to have_content ticket.name
+    end
+  end
+
+  scenario "with invalid attributes" do
+    fill_in "Name",	with: ""
+    click_button "Update Ticket"
+    
+    expect(page).to have_content "Ticket has not been updated."
+  end
+end
+      
