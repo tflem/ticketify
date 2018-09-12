7bbc5236ab2d581b9d4aa3d786d3e8e89020b3a7
Tim Fleming <tfleming26@hotmail.com>
2018-09-12 17:17:42 -0600

    Create admin namespace with app controller

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -17,24 +17,24 @@ RSpec.feature "Users can create tickets" do
 
     expect(page).to have_content "Ticket has been created."
     within("#ticket") do
-      expect(page).to have_content "User: #{user.email}"
+      expect(page).to have_content user.email
     end
   end
     
-    scenario "when providing invalid attributes" do
-      click_button "Create Ticket"
+  scenario "when providing invalid attributes" do
+    click_button "Create Ticket"
 
-      expect(page).to have_content "Ticket has not been created."
-      expect(page).to have_content "Name can't be blank"
-      expect(page).to have_content "Description can't be blank"
-    end
-
-    scenario "with an invalid description" do
-      fill_in "Name",	with: "Non-standard compliance"
-      fill_in "Description",	with: "Too slow"
-      click_button "Create Ticket"
-      
-      expect(page).to have_content "Ticket has not been created."
-      expect(page).to have_content "Description is too short"
-    end
+    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Description can't be blank"
   end
+
+  scenario "with an invalid description" do
+    fill_in "Name",	with: "Non-standard compliance"
+    fill_in "Description",	with: "Too slow"
+    click_button "Create Ticket"
+      
+    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Description is too short"
+  end
+end
